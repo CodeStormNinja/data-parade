fe7daf8452da126aec1da488381945706c7142c5
HenriBDev <henribdev@gmail.com>
2025-09-05 22:58:40 -0300

    ✨ feat - Added Swagger with flask-restx

--- main/app.py
+++ main/app.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_restx import Api, Namespace
+
 from main.config.Config import Config
+from main.routes.WeatherForecastRoute import ns as weather_forecast_ns
 
 def create_app():
     
@@ -7,5 +10,17 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config())
     app.config.from_object(Config.get_environment_config())
+    
+    # SWAGGER
+    api = Api(
+        app=app, 
+        version="1.0", 
+        title="NASA Space Apps Challenge 2025 - Will it rain on my parade?", 
+        description="Weather Forecast API", 
+        doc="/docs"
+    )
+    
+    # ROUTES
+    api.add_namespace(weather_forecast_ns)
 
     return app
